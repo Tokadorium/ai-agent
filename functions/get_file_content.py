35e91f193942c72e2dcdb7b7d0bf652aefb93ad2
jakubrejmak <jakubrejmak@gmail.com>
2025-06-22 11:02:21 +0200

    finished get_file_content, added tests for it too

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -21,5 +21,10 @@ def get_file_content(working_directory, file_path):
         with open(path, "r") as f:
             file_content = f.read(MAX_CHARS)
 
+        if len(file_content) == MAX_CHARS:
+            file_content += f'[...File "{file_path}" truncated at 10000 characters]'
+
+        return file_content
+
     except Exception as e:
         print(f"Error: {e}")
