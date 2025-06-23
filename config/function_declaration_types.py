d8d09017e7230932f5300e9eb4f14de0b8e70233
jakubrejmak <jakubrejmak@gmail.com>
2025-06-23 08:07:45 +0200

    added function executing for LLM

---
+++ config/function_declaration_types.py
@@ -0,0 +1,69 @@
+from google.genai import types
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Lists the content of the file in the specified directory, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to list content of, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes the provided content to the specified file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to write to, relative to the working directory. If file doesn't exist it will be created. If file is already present it will be overwritten.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING, description="String of text to write to file"
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Executes a .py file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to execute, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_write_file,
+        schema_run_python_file
+    ]
+)
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from config.function_declaration_types import available_functions
+from config.system_prompt import system_prompt
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -23,17 +26,23 @@ for arg in sys.argv:
     if arg in args:
         args[arg] = True
 
-system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT"
-
 messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
     contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt)
+    config=types.GenerateContentConfig(
+        system_instruction=system_prompt, tools=[available_functions]
+    ),
 )
 
-print(response.text)
+func_calls = response.function_calls
+
+if func_calls:
+    for call in func_calls:
+        print(f"Calling function: {call.name}{call.args}")
+else:
+    print(response.text)
 
 if args["--verbose"]:
     print(f"User prompt: {user_prompt}")
