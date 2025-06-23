d9f4aac21faf0efe5543074dffddfd45fbf5264a
jakubrejmak <jakubrejmak@gmail.com>
2025-06-23 10:37:57 +0200

    removed calculator test dir, added response loop, cleaned main.py

--- main.py
+++ main.py
@@ -24,23 +24,38 @@ for arg in sys.argv:
     if arg in args:
         args[arg] = True
 
-messages = [types.Content(role="user", parts=[types.Part(text=args["user_prompt"])])]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=args["user_prompt"])]),
+]
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-    config=types.GenerateContentConfig(
-        system_instruction=system_prompt, tools=[available_functions]
-    ),
-)
+for i in range(0, 20):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, tools=[available_functions]
+        ),
+    )
 
-function_calls_list = response.function_calls
-function_call_result = call_function(function_calls_list[0], args["--verbose"])
+    for candidate in response.candidates:
+        messages.append(candidate.content)
 
-if not function_call_result.parts[0].function_response.response:
-    raise Exception("Fatal error: invalid response object")
-if args["--verbose"]:
-    print(f"-> {function_call_result.parts[0].function_response.response}")
-    print(f"User prompt: {args["user_prompt"]}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    function_calls_list = response.function_calls
+
+    if function_calls_list:
+        function_call_result = call_function(function_calls_list[0], args["--verbose"])
+
+        if not function_call_result.parts[0].function_response.response:
+            raise Exception("Fatal error: invalid response")
+
+        if args["--verbose"]:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        messages.append(function_call_result)
+    else:
+        print(response.text)
+        if args["--verbose"]:
+            print(f"User prompt: {args["user_prompt"]}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        break
